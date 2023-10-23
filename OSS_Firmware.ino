24dd4ad255104bea8cdcb5e6769cd6c6640c52cc
Kang Myeong Seok <tametabby1@naver.com>
2023-10-23 13:24:56 +0800

    2023-10-23
    Final Modification

--- OSS_Firmware.ino
+++ OSS_Firmware.ino
@@ -1,175 +1,134 @@
-#if !defined(ESP8266)
-#error This code is designed to run on ESP8266 and ESP8266-based boards! Please check your Tools->Board setting.
-#endif
-
+#include <Ticker.h>
 #include <Wire.h>
 #include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 #include <AHTxx.h>
-#include "config.h"
 #include "ESP8266TimerInterrupt.h"
 
-#define HUMIDIFIER 2
+#define HUMIDIFIER 15
 #define SOIL_SENSOR A0
+#define BUFF_SIZE 50
 
-AdafruitIO_Feed *temp_val = io.feed("temperature_value");
-AdafruitIO_Feed *temp_send = io.feed("temperature_send");
-AdafruitIO_Feed *humi_val = io.feed("humidity_value");
-AdafruitIO_Feed *humi_send = io.feed("humidity_send");
-AdafruitIO_Feed *soil_val = io.feed("soil_moisture_value");
-AdafruitIO_Feed *soil_send = io.feed("soil_moisture_send");
-AdafruitIO_Feed *humidifier = io.feed("humidifier");
+unsigned long lastMsg = 0;
 
-AHTxx aht10(AHTXX_ADDRESS_X38, AHT1x_SENSOR); //AHT15 Initializing
-
+const char* ssid = "wi_dje21";
+const char* password = "Djedsmhspw2015!";
+const char* mqtt_server = "broker.mqtt-dashboard.com";
 
 float ahtValue = 0, ahtTemp = 0, ahtHumi = 0;
 int soilValue;
-int std_temp, std_humi, std_soil, humidifier_state;
+int std_temp, std_humi, std_soil;
 
-void setup() {
-  pinMode(HUMIDIFIER, OUTPUT);
-  Serial.begin(115200);
+uint8_t sensor_state = 1;                           // 초기값 : 1, 1~3 범위
 
-  digitalWrite(HUMIDIFIER, LOW);
+char temp_buf[50];
+char humi_buf[50];
+char soil_buf[50];
 
-  while (! Serial && millis() < 5000); // Serial Enable Checking
+AHTxx aht10(AHTXX_ADDRESS_X38, AHT1x_SENSOR); //AHT15 Initializing
+Ticker timer;
 
-  //-----------------------------------AHT15 code---------------------------------------------
+WiFiClient espClient;
+PubSubClient client(espClient);
 
-  while (aht10.begin() != true) //for ESP-01 use aht10.begin(0, 2);
-  {
-    Serial.println(F("AHT1x not connected or fail to load calibration coefficient")); //(F()) save string to flash & keeps dynamic memory free
-
+void operate_humi() {
+  if (std_temp > ahtTemp && std_humi < ahtHumi) {
+    Serial.print("Humidifier can't work now!");
+    digitalWrite(HUMIDIFIER, LOW);
+  }
+  else {
+    digitalWrite(HUMIDIFIER, HIGH);
+    Serial.print("Humidifier turnning ON\n");
     delay(5000);
+    digitalWrite(HUMIDIFIER, LOW);
   }
+}
 
-  Serial.println(F("AHT10 OK"));
+//----------------------------------MQTT Connection----------------------------
+void setup_wifi() {
 
-  Serial.print("Connecting to Adafruit IO");
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
-  //--------------------------------Adafruit IO code------------------------------------------
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
 
-  io.connect();
-
-  temp_send->onMessage(temp_message);
-  humi_send->onMessage(humi_message);
-  soil_send->onMessage(soil_message);
-  humidifier->onMessage(humidifier_message);
-
-  while (io.status() < AIO_CONNECTED) {
-    Serial.print(".");
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
+    Serial.print(".");
   }
 
-  //connected
-  Serial.println();
-  Serial.println(io.statusText());
+  randomSeed(micros());
 
-  temp_send->get();
-  humi_send->get();
-  soil_send->get();
-  humidifier->get();
-
-  //-------------------------------------------------------------------------------------------
-  digitalWrite(HUMIDIFIER, LOW);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
+void callback(char* topic, byte* payload, unsigned int length) {
+  static String myTopic = topic;
+  static String myPayload = (char *)payload;
 
-void loop() {
-  io.run();
-  measureSoilMoisture();
-  measureTempHumi();
-  sendTempHumiSoil();
-  operateFarm();
-  delay(2000);
+  Serial.print("Message arrived [");
+  Serial.print(myTopic);
+  Serial.print("] :");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  //--------------------------Topic, Payload-------------------------------
+
+  if (myTopic.equals("MyeongEi/OSS/Temp/in")) {
+    std_temp = myPayload.toInt();
+    Serial.print("\nStandard Temp : ");
+    Serial.println(std_temp);
+  }
+  else if (myTopic.equals("MyeongEi/OSS/Humi/in")) {
+    std_humi = myPayload.toInt();
+    Serial.print("Standard Humi : ");
+    Serial.println(std_humi);
+  }
+  else if (myTopic.equals("MyeongEi/OSS/Soil_Moisture/in")) {
+    std_soil = myPayload.toInt();
+    Serial.print("Standard Soil Moisture : ");
+    Serial.println(std_soil);
+  }
+  else if (myTopic.equals("MyeongEi/OSS/Humidifier/in") && payload[0] == '1') {
+    operate_humi();
+  }
 }
 
-void measureTempHumi() {
-  Serial.println();
-  //Serial.println(F("DEMO 1: read 12-bytes"));
+//-----------------------------------------------------------------------
 
-  ahtValue = aht10.readTemperature(); //read 6-bytes via I2C, takes 80 milliseconds
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+      client.publish("outTopic", "hello world");
 
-  Serial.print(F("Temperature...: "));
+      //------------Subscribe area----------------
 
-  if (ahtValue != AHTXX_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
-  {
-    ahtTemp = ahtValue;
-    Serial.print(ahtValue);
-    Serial.println(F(" +-0.3C"));
+      client.subscribe("MyeongEi/OSS/Temp/in");
+      client.subscribe("MyeongEi/OSS/Humi/in");
+      client.subscribe("MyeongEi/OSS/Soil_Moisture/in");
+      client.subscribe("MyeongEi/OSS/Humidifier/in");
+
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
   }
-  else
-  {
-    printStatus(); //print temperature command status
-
-    if   (aht10.softReset() == true) Serial.println(F("reset success")); //as the last chance to make it alive
-    else                             Serial.println(F("reset failed"));
-  }
-
-
-  //==================================================================================
-    delay(2000); //measurement with high frequency leads to heating of the sensor, see NOTE
-  //==================================================================================
-
-  ahtValue = aht10.readHumidity(); //read another 6-bytes via I2C, takes 80 milliseconds
-
-  Serial.print(F("Humidity......: "));
-
-  if (ahtValue != AHTXX_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
-  {
-    ahtHumi = ahtValue;
-    Serial.print(ahtValue);
-    Serial.println(F(" +-2%"));
-  }
-  else
-  {
-    printStatus(); //print humidity command status
-  }
-
-
-  //==================================================================================
-    delay(2000); //measurement with high frequency leads to heating of the sensor, see NOTE
-  //==================================================================================
-
-  /* DEMO - 2, temperature call will read 6-bytes via I2C, humidity will use same 6-bytes */
-
-  Serial.println();
-  //Serial.println(F("DEMO 2: read 6-byte"));
-
-  ahtValue = aht10.readTemperature(); //read 6-bytes via I2C, takes 80 milliseconds
-
-  Serial.print(F("Temperature: "));
-
-  if (ahtValue != AHTXX_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
-  {
-    ahtTemp = ahtValue;
-    Serial.print(ahtValue);
-    Serial.println(F(" +-0.3C"));
-  }
-  else
-  {
-    printStatus(); //print temperature command status
-  }
-
-  ahtValue = aht10.readHumidity(AHTXX_USE_READ_DATA); //use 6-bytes from temperature reading, takes zero milliseconds!!!
-
-  Serial.print(F("Humidity...: "));
-
-  if (ahtValue != AHTXX_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
-  {
-    ahtHumi = ahtValue;
-    Serial.print(ahtValue);
-    Serial.println(F(" +-2%"));
-  }
-  else
-  {
-    printStatus(); //print temperature command status not humidity!!! RH measurement use same 6-bytes from T measurement
-  }
-
-  //==================================================================================
-    delay(2000); //recomended polling frequency 8sec..30sec
-  //==================================================================================
 }
+//================================================================================
 
 void printStatus()
 {
@@ -201,51 +160,161 @@ void printStatus()
   }
 }
 
-void sendTempHumiSoil() {
-  temp_val->save(ahtTemp);
-  humi_val->save(ahtHumi);
-  soil_val->save(soilValue);
+void measureTempHumi() {
+  if (sensor_state == 1) {
+
+    /* DEMO - 1, every temperature or humidity call will read 6-bytes over I2C, total 12-bytes */
+    Serial.println();
+    Serial.println(F("DEMO 1: read 12-bytes"));
+
+    ahtValue = aht10.readTemperature(); //read 6-bytes via I2C, takes 80 milliseconds
+
+    Serial.print(F("Temperature...: "));
+
+    if (ahtValue != AHTXX_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
+    {
+      ahtTemp = ahtValue;
+      Serial.print(ahtValue);
+      Serial.println(F(" +-0.3C"));
+    }
+    else
+    {
+      printStatus(); //print temperature command status
+
+      if   (aht10.softReset() == true) Serial.println(F("reset success")); //as the last chance to make it alive
+      else                             Serial.println(F("reset failed"));
+    }
+
+    sensor_state++;
+    timer.attach_ms(2000, measureTempHumi);
+  }
+
+  else if (sensor_state == 2) {
+
+    ahtValue = aht10.readHumidity(); //read another 6-bytes via I2C, takes 80 milliseconds
+
+    Serial.print(F("Humidity......: "));
+
+    if (ahtValue != AHTXX_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
+    {
+      ahtHumi = ahtValue;
+      Serial.print(ahtValue);
+      Serial.println(F(" +-2%"));
+    }
+    else
+    {
+      printStatus(); //print humidity command status
+    }
+
+    sensor_state++;
+    timer.attach_ms(2000, measureTempHumi);
+  }
+
+  else {
+    timer.attach_ms(500, measureTempHumi);
+    /* DEMO - 2, temperature call will read 6-bytes via I2C, humidity will use same 6-bytes */
+    Serial.println();
+    Serial.println(F("DEMO 2: read 6-byte"));
+
+    ahtValue = aht10.readTemperature(); //read 6-bytes via I2C, takes 80 milliseconds
+
+    Serial.print(F("Temperature: "));
+
+    if (ahtValue != AHTXX_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
+    {
+      ahtTemp = ahtValue;
+      Serial.print(ahtValue);
+      Serial.println(F(" +-0.3C"));
+    }
+    else
+    {
+      printStatus(); //print temperature command status
+    }
+
+    ahtValue = aht10.readHumidity(AHTXX_USE_READ_DATA); //use 6-bytes from temperature reading, takes zero milliseconds!!!
+
+    Serial.print(F("Humidity...: "));
+
+    if (ahtValue != AHTXX_ERROR) //AHTXX_ERROR = 255, library returns 255 if error occurs
+    {
+      ahtHumi = ahtValue;
+      Serial.print(ahtValue);
+      Serial.println(F(" +-2%"));
+    }
+    else
+    {
+      printStatus(); //print temperature command status not humidity!!! RH measurement use same 6-bytes from T measurement
+    }
+
+    sensor_state = 1;
+    timer.attach_ms(8000, measureTempHumi);
+  }
 }
 
 void measureSoilMoisture() {
-  soilValue = analogRead(SOIL_SENSOR);
+  soilValue = map(analogRead(SOIL_SENSOR), 0, 1023, 0, 100);
   Serial.print("Soil Moisture Value : ");
   Serial.println(soilValue);
 }
 
-
-
-void temp_message(AdafruitIO_Data *data) {
-  std_temp = data->toInt();
-  Serial.print("Standard Temperature : ");
-  Serial.println(std_temp);
-}
-
-void humi_message(AdafruitIO_Data *data) {
-  std_humi = data->toInt();
-  Serial.print("Standard Humidity : ");
-  Serial.println(std_humi);
-}
-
-void soil_message(AdafruitIO_Data *data) {
-  std_soil = data->toInt();
-  Serial.print("Standard Soil Moisture : ");
-  Serial.println(std_soil);
-}
-
-void humidifier_message(AdafruitIO_Data *data) {
-  humidifier_state = data->toPinLevel();
-  Serial.print("Humidifier Control Switch : ");
-  Serial.println(humidifier_state);
-  digitalWrite(HUMIDIFIER, humidifier_state);
-}
-
-void operateFarm(){
-  if(ahtTemp > std_temp && ahtHumi < std_humi && soilValue < std_soil){
+void operateFarm() {
+  if (ahtTemp > std_temp && ahtHumi < std_humi && soilValue < std_soil) {
+    digitalWrite(HUMIDIFIER, HIGH);
+  }
+  else if (ahtTemp < std_temp && ahtHumi > std_humi && soilValue > std_soil) {
     digitalWrite(HUMIDIFIER, LOW);
   }
-  //-------------------------------------------
-  else if(ahtTemp < std_temp && ahtHumi > std_humi && soilValue > std_soil){
-    digitalWrite(HUMIDIFIER, HIGH);
+}
+
+void setup() {
+  pinMode(HUMIDIFIER, OUTPUT);
+  Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+
+  digitalWrite(HUMIDIFIER, LOW);
+
+  while (! Serial && millis() < 5000);
+
+  //-----------------------------------AHT15 code---------------------------------------------
+
+  while (aht10.begin() != true)
+  {
+    Serial.println(F("AHT1x not connected or fail to load calibration coefficient")); //(F()) save string to flash & keeps dynamic memory free
+
+    delay(5000);
+  }
+
+  Serial.println(F("AHT10 OK"));
+  sensor_state = 1;
+  timer.attach_ms(2000, measureTempHumi);
+
+  //------------------------------------------------------------------------------------------
+}
+
+
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  //operateFarm();
+
+  unsigned long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    measureSoilMoisture();
+
+    //-------------------Publish area--------------------
+    snprintf(temp_buf, BUFF_SIZE, "%.1f", ahtTemp);
+    snprintf(humi_buf, BUFF_SIZE, "%.1f", ahtHumi);
+    snprintf(soil_buf, BUFF_SIZE, "%d", soilValue);
+    client.publish("MyeongEi/OSS/Temp/out", temp_buf);
+    client.publish("MyeongEi/OSS/Humi/out", humi_buf);
+    client.publish("MyeongEi/OSS/Soil_Moisture/out", soil_buf);
   }
 }
